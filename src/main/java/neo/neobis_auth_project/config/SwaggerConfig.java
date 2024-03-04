f6b9b88c2750daf9513578c28d84166fa6425085
Nurik <nalymbaide@gmail.com>
2024-03-04 18:51:24 +0600

    check swagger

--- src/main/java/neo/neobis_auth_project/config/SwaggerConfig.java
+++ src/main/java/neo/neobis_auth_project/config/SwaggerConfig.java
@@ -1,35 +1,35 @@
-package neo.neobis_auth_project.config;
-
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Collections;
-
-@Configuration
-public class SwaggerConfig implements WebMvcConfigurer {
-    private static final String API_KEY = "Bearer Token";
-
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .components(new Components()
-                        .addSecuritySchemes(API_KEY, apiKeySecuritySchema()))
-                .info(new Info().title("Neo-Auth-Project"))
-                .security(Collections.singletonList(new SecurityRequirement().addList(API_KEY)));
-    }
-
-    public SecurityScheme apiKeySecuritySchema() {
-        return new SecurityScheme()
-                .name("Bearer Token")
-                .description("Please put the token")
-                .in(SecurityScheme.In.HEADER)
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("Bearer");
-    }
-}
+//package neo.neobis_auth_project.config;
+//
+//import io.swagger.v3.oas.models.Components;
+//import io.swagger.v3.oas.models.OpenAPI;
+//import io.swagger.v3.oas.models.info.Info;
+//import io.swagger.v3.oas.models.security.SecurityRequirement;
+//import io.swagger.v3.oas.models.security.SecurityScheme;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import java.util.Collections;
+//
+//@Configuration
+//public class SwaggerConfig implements WebMvcConfigurer {
+//    private static final String API_KEY = "Bearer Token";
+//
+//    @Bean
+//    public OpenAPI customOpenAPI() {
+//        return new OpenAPI()
+//                .components(new Components()
+//                        .addSecuritySchemes(API_KEY, apiKeySecuritySchema()))
+//                .info(new Info().title("Neo-Auth-Project"))
+//                .security(Collections.singletonList(new SecurityRequirement().addList(API_KEY)));
+//    }
+//
+//    public SecurityScheme apiKeySecuritySchema() {
+//        return new SecurityScheme()
+//                .name("Bearer Token")
+//                .description("Please put the token")
+//                .in(SecurityScheme.In.HEADER)
+//                .type(SecurityScheme.Type.HTTP)
+//                .scheme("Bearer");
+//    }
+//}
